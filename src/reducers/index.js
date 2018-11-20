ed7c3022081d6d326730c1a7dc4495e978778935
Wang Biqing <wang_bi@worksap.co.jp>
2018-11-20 18:45:26 +0800

    refe #000000 reducer refine
    (cherry picked from commit 8ffbfe2)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,15 +4,23 @@ import {
   DISABLE_PREVIOUS,
   ENABLE_PREVIOUS,
   TURN_ON_PLAYER,
-  TURN_OFF_PLAYER, SET_PLAYLISTS, SET_PLAYLIST, SET_LIST_NAME, SET_LOG_IN, SET_LOG_OUT, SET_PLAY_MODE
+  TURN_OFF_PLAYER,
+  SET_PLAYLISTS,
+  SET_PLAYLIST,
+  SET_LIST_NAME,
+  SET_LOG_IN,
+  SET_LOG_OUT,
+  SET_PLAY_MODE,
+  SET_PEEK_LIST,
+  SET_CURRENT_SONG
 } from "../actions/actions";
-import {DEMO_SONG, PLAY_MODE, DEFAULT_PLAYLISTS} from "../constants/states";
+import {DEMO_SONG, PLAY_MODE, DEFAULT_PLAYLISTS, MOCK_SONGS} from "../constants/states";
 
 const defaultState = {
   playList:[DEMO_SONG],
   playMode: PLAY_MODE.SHUFFLE,
   isPlayModeChanged: false,
-  peekList:[DEMO_SONG],
+  peekList:MOCK_SONGS,
   isTurnedOn: false,
   isPreviousEnabled:false,
   currentSong:DEMO_SONG,
@@ -23,13 +31,8 @@ const defaultState = {
 
 export default function appData(state = defaultState, action) {
   switch (action.type) {
-    case SET_NEXT:
-      return {
-        ...state,
-        currentSong: action.song,
-      };
-    case SET_PREVIOUS:
-      return {
+    case SET_CURRENT_SONG:
+      return{
         ...state,
         currentSong: action.song,
       };
@@ -42,37 +45,37 @@ export default function appData(state = defaultState, action) {
       return {
         ...state,
         isPreviousEnabled:true,
-      }
+      };
     case TURN_ON_PLAYER:
       return{
         ...state,
         isTurnedOn:true,
-      }
+      };
     case TURN_OFF_PLAYER:
       return{
         ...state,
         isTurnedOn:false,
-      }
+      };
     case SET_PLAYLISTS:
       return{
         ...state,
         playLists:action.playlists,
-      }
+      };
     case SET_PLAYLIST:
       return{
         ...state,
-        playList:action.playList,
-      }
+        playList:action.playlist,
+      };
     case SET_LIST_NAME:
       return {
         ...state,
         listName:action.listName,
-    }
+    };
     case SET_LOG_IN:
       return{
         ...state,
         isLoggedIn:true,
-      }
+      };
     case SET_LOG_OUT:
       return{
         ...state,
@@ -81,12 +84,17 @@ export default function appData(state = defaultState, action) {
         isLoggedIn:false,
         peekList:[DEMO_SONG],
         playLists:DEFAULT_PLAYLISTS,
-      }
+      };
     case SET_PLAY_MODE:
       return{
         ...state,
         playMode:action.playMode,
-      }
+      };
+    case SET_PEEK_LIST:
+      return{
+        ...state,
+        peekList:action.peekList
+      };
     default:
       return state
   }
